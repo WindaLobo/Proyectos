6dee6b2bdfbf37c66f4586a1efa50f186d23008f
Adrián Estévez <Adrianestevezgayoso@gmail.com>
2022-11-04 22:40:50 +0100

    Practica con String

---
+++ practica/src/main/java/com/mycompany/practica/Prueba.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.practica;
+
+
+public class Prueba {
+     public static void main(String[]args) {
+         AnalizadorDeCuentas obx = new AnalizadorDeCuentas();
+         obx.cuentaBancaria1();
+         
+    
+}
+}
