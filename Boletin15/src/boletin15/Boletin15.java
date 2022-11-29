d68be5eab6b6bd7b7bdfbe6d2f8baa974a894a9b
Windar Lobo <wlobogarces@danielcastelao.org>
2022-11-29 10:37:10 +0100

    actualizacion

--- Boletin15/src/boletin15/Boletin15.java
+++ Boletin15/src/boletin15/Boletin15.java
@@ -2,28 +2,50 @@ package boletin15;
 
 import javax.swing.JOptionPane;
 
+
 public class Boletin15 {
+    /**
+     *
+     * @param args
+     */
 
     public static void main(String[] args) {
 
-        String genero = JOptionPane.showInputDialog("Escribe un genero musical");
-        String titulo = JOptionPane.showInputDialog("Escribe un titulo musical");
-        String autor = JOptionPane.showInputDialog("Escribe un autor musical");
-        String formato = JOptionPane.showInputDialog("Escribe un formato musical");
+/**
+ *Introducimos el mensaje
+ */
+        String genero=pedirString("Escribe un genero");
+        String titulo = pedirString("Escribe un titulo musical");
+        String autor = pedirString("Escribe un autor musical");
+        String formato = pedirString("Escribe un formato musical");
         
-         String actorPrincipal= JOptionPane.showInputDialog("Escribe un actorPrincipal");
-        String actrizPrincipal= JOptionPane.showInputDialog("Escribe una  actrizPrincipal");
-        String tituloPelicula = JOptionPane.showInputDialog("Escribe un titulo de la pelicula");
-        String  autorPelicula = JOptionPane.showInputDialog("Escribe un autor de la pelicula ");
-        String formatoPelicula = JOptionPane.showInputDialog("Escribe un formato de la pelicula ");
-
+         String actorPrincipal= pedirString("Escribe un actorPrincipal");
+        String actrizPrincipal= pedirString("Escribe una  actrizPrincipal");
+        String tituloPelicula = pedirString("Escribe un titulo de la pelicula");
+        String  autorPelicula = pedirString("Escribe un autor de la pelicula ");
+        String formatoPelicula = pedirString("Escribe un formato de la pelicula ");
+/**
+ * utilizamos new para crear objecto
+ */
         Pelicula peli = new Pelicula( actorPrincipal, actrizPrincipal,  tituloPelicula, autorPelicula, formatoPelicula, 126.00f);
         System.out.println("La pelicula es \n " + peli.toString());
-        
-        
+
+        /**
+         * utilizamos new para crear objecto
+         */
         Disco disco = new Disco(genero, titulo, autor, formato, 1.30f);
         System.out.println("El disco es \n " + disco.toString());
 
+    }
+
+    /**
+     * Este metodo lo q hacer es retornar el mensaje vamos a pedir
+     * @param mensaje
+     * @return mensaje
+     */
+    public static  String pedirString(String mensaje ){
+        return JOptionPane.showInputDialog(mensaje );
+        
     }
 
 }
