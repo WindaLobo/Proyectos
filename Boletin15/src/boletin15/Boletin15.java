ea78b632534e1d3565865049fd9f2f66cf25dc27
Windar Lobo <wlobogarces@danielcastelao.org>
2022-12-12 10:14:19 +0100

    Actualizacion

--- Boletin15/src/boletin15/Boletin15.java
+++ Boletin15/src/boletin15/Boletin15.java
@@ -2,18 +2,11 @@ package boletin15;
 
 import javax.swing.JOptionPane;
 
-
 public class Boletin15 {
-    /**
-     *
-     * @param args
-     */
 
     public static void main(String[] args) {
+      
 
-/**
- *Introducimos el mensaje
- */
         String genero=pedirString("Escribe un genero");
         String titulo = pedirString("Escribe un titulo musical");
         String autor = pedirString("Escribe un autor musical");
@@ -24,28 +17,18 @@ public class Boletin15 {
         String tituloPelicula = pedirString("Escribe un titulo de la pelicula");
         String  autorPelicula = pedirString("Escribe un autor de la pelicula ");
         String formatoPelicula = pedirString("Escribe un formato de la pelicula ");
-/**
- * utilizamos new para crear objecto
- */
+
         Pelicula peli = new Pelicula( actorPrincipal, actrizPrincipal,  tituloPelicula, autorPelicula, formatoPelicula, 126.00f);
         System.out.println("La pelicula es \n " + peli.toString());
-
-        /**
-         * utilizamos new para crear objecto
-         */
+        
+        
         Disco disco = new Disco(genero, titulo, autor, formato, 1.30f);
         System.out.println("El disco es \n " + disco.toString());
 
     }
-
-    /**
-     * Este metodo lo q hacer es retornar el mensaje vamos a pedir
-     * @param mensaje
-     * @return mensaje
-     */
     public static  String pedirString(String mensaje ){
         return JOptionPane.showInputDialog(mensaje );
         
     }
 
-}
+} 
